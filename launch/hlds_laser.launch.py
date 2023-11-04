02fc3bc6c621eb6ad0b2b2b67e73e6fcaf551539
Prof. Just <olaf.just@w-hs.de>
2023-11-04 16:13:04 +0100

    Update hlds_laser.launch.py

--- launch/hlds_laser.launch.py
+++ launch/hlds_laser.launch.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Darby Lim, Pyo
+
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -27,8 +45,8 @@ def generate_launch_description():
 
         Node(
             package='hls_lfcd_lds_driver',
-            node_executable='hlds_laser_publisher',
-            node_name='hlds_laser_publisher',
+            executable='hlds_laser_publisher',
+            name='hlds_laser_publisher',
             node_namespace='tb3_0',  # <------------------- ADD THIS!
             parameters=[{'port': port, 'frame_id': frame_id}],
             output='screen'),
